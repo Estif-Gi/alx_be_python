7917991f1dc6a6a8b560cde4bef652ca4661aba6
GebetaEst <estifanosgirma4@gmail.com>
2025-11-09 21:08:01 +0300

    i

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
-principal = 1000
+P = 1000
 
-rate = 0.05
+R = 0.05
 
-time = 3
+T = 3
 
 # now we calculate the simple interest with this formula 
-I = (principal * rate * time)
+I = (P * R * T)
 
 print("The simple interest is:",I)
