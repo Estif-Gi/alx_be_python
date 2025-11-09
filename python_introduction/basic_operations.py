f2bcaf1cdfbcf16c87e2706cfeff1f9c0d909faa
GebetaEst <estifanosgirma4@gmail.com>
2025-11-09 20:41:26 +0300

    first python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 11
+
+addition = number1 + number2
+
+subtraction = number1 - number2
+
+multiplication = number1 * number2
+
+print("this it the addition of 10 and 11" , addition)
+
+print("this it the subtraction of 10 and 11" , subtraction)
+
+print("this it the multiplication of 10 and 11" , multiplication)
+
