462273d618bca39b3c73cbced28808ac9c2fdfac
GebetaEst <estifanosgirma4@gmail.com>
2025-11-29 21:28:22 +0300

    2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
 multiple = 0
-for iter in range(1,10):
+for iter in [1,2,3,4,5,6,7,8,9,10]:
    multiple = iter * number
    print(f"{number} * {iter} = {multiple} ")
