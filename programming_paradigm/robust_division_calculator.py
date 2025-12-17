65f29c60fabb9058e46b627493823cb4a28d7112
GebetaEst <estifanosgirma4@gmail.com>
2025-12-17 12:26:57 +0300

    s

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def  safe_divide(numerator, denominator):
     try:
         result = float(numerator) / float(denominator)
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Invalid input value."
     return result
