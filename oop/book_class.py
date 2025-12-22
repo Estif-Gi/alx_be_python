682deffee67f594c954230becc2964ca76c672b3
GebetaEst <estifanosgirma4@gmail.com>
2025-12-22 16:49:04 +0300

    new

---
+++ oop/book_class.py
@@ -0,0 +1,12 @@
+
+class Book:
+    def __init__(self, title, author , year):
+        self.title = title
+        self.author = author
+        self.year_published = year
+    def __del__(self):
+        return(f"Deleting (title of the book): {self.title}")
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year_published}"
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year_published})"
